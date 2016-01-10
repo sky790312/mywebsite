899632f2c0748a150fdbe078ae842a105e0b6ff0
kevin hu <sky790312yks@gmail.com>
2016-01-11 01:56:57 +0800

    add style console text

--- public/sources/scripts/indexBoot.js
+++ public/sources/scripts/indexBoot.js
@@ -11,3 +11,17 @@ import index from '../scripts/index.js';
 window.index = index;
 
 new index($('#app'));
+
+// style console
+let consoleText = "welcome to KevinHu's website!";
+let consoleStyle = '\
+						font-size: 20px; \
+						background-color: red; \
+						font-weight: bold; \
+						color: white; \
+						text-shadow: 0 0 1em black, 0 0 1em black, 0 0 1em black, 0 0 1em black, 0 0 1em black; \
+						padding: 3px 5%; \
+						border: .1em solid rgba(0,0,0,.4); \
+						border-radius: 1em; \
+					';
+console.log('%c' + consoleText, consoleStyle);
